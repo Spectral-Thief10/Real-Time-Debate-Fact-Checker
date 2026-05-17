26e2e0580646a93d2614cb8d1bf43a003da59762
Spectral-Thief10 <148111599+Spectral-Thief10@users.noreply.github.com>
2026-05-17 11:32:13 -0700

    Delete transcription directory

--- transcription/server.py
+++
@@ -1,8 +0,0 @@
-# server.py
-import os
-os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
-os.environ["OMP_NUM_THREADS"] = "1"
-from whisper_live.server import TranscriptionServer
-
-server = TranscriptionServer()
-server.run(host="0.0.0.0", port=9090, backend="faster_whisper")
